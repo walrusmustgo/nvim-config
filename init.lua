2fcc5a55fb1b33f73311bd29c59ae54887b8b8ab
walrusmustgo <markokovachki@gmail.com>
2024-07-02 12:39:10 +0200

    prolly added gdscript

--- init.lua
+++ init.lua
@@ -580,6 +580,7 @@ require('lazy').setup({
         glsl_analyzer = {
           filetypes = { 'glsl', 'vert', 'frag', 'comp' },
         },
+        gdtoolkit = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
